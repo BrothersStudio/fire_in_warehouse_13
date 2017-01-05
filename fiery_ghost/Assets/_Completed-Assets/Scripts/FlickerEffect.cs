987531975cd777daa8bcec16817b1f98b5775ad6
whatPanache <pwcarney@bu.edu>
2017-01-05 00:15:13 -0500

    Added random probably useless tutorial assets and set camera and fire effect tests

---
+++ fiery_ghost/Assets/_Completed-Assets/Scripts/FlickerEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickerEffect : MonoBehaviour {
+
+	public GameObject redfire;
+	public GameObject yellowfire; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
