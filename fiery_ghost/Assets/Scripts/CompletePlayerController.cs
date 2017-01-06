1fb7040c0c56c150f148ad15bf571f51b2fb0869
whatPanache <pwcarney@bu.edu>
2017-01-05 20:45:26 -0500

    Baby's first water shooting mechanic

--- fiery_ghost/Assets/Scripts/CompletePlayerController.cs
+++ fiery_ghost/Assets/Scripts/CompletePlayerController.cs
@@ -7,26 +7,28 @@ using UnityEngine.UI;
 public class CompletePlayerController : MonoBehaviour {
 
 	public float speed;				//Floating point variable to store the player's movement speed.
-	public Text countText;			//Store a reference to the UI Text component which will display the number of pickups collected.
-	public Text winText;			//Store a reference to the UI Text component which will display the 'You win' message.
 
+	public GameObject water;
+	public Transform waterSpawn;
+	public float fireRate;
+
+	private float nextFire;
 	private Rigidbody2D rb2d;		//Store a reference to the Rigidbody2D component required to use 2D Physics.
-	private int count;				//Integer to store the number of pickups collected so far.
 
 	// Use this for initialization
 	void Start()
 	{
 		//Get and store a reference to the Rigidbody2D component so that we can access it.
 		rb2d = GetComponent<Rigidbody2D> ();
+	}
 
-		//Initialize count to zero.
-		count = 0;
-
-		//Initialze winText to a blank string since we haven't won yet at beginning.
-		winText.text = "";
-
-		//Call our SetCountText function which will update the text with the current value for count.
-		SetCountText ();
+	void Update()
+	{
+		if (Input.GetMouseButton (0) && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate (water, waterSpawn.position, waterSpawn.rotation);
+		}
 	}
 
 	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
@@ -51,28 +53,9 @@ public class CompletePlayerController : MonoBehaviour {
 		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
 		if (other.gameObject.CompareTag ("PickUp")) 
 		{
-			//... then set the other object we just collided with to inactive.
-			other.gameObject.SetActive(false);
-			
-			//Add one to the current value of our count variable.
-			count = count + 1;
-			
-			//Update the currently displayed count by calling the SetCountText function.
-			SetCountText ();
+
 		}
+
+	}
 		
-
-	}
-
-	//This function updates the text displaying the number of objects we've collected and displays our victory message if we've collected all of them.
-	void SetCountText()
-	{
-		//Set the text property of our our countText object to "Count: " followed by the number stored in our count variable.
-		countText.text = "Count: " + count.ToString ();
-
-		//Check if we've collected all 12 pickups. If we have...
-		if (count >= 12)
-			//... then set the text property of our winText object to "You win!"
-			winText.text = "You win!";
-	}
 }
---
+++ fiery_ghost/Assets/Scripts/MovingWater.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWater : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Get and store a reference to the Rigidbody2D component so that we can access it.
+		rb2d = GetComponent<Rigidbody2D> ();
+
+		rb2d.AddForce (transform.up * speed);
+	}
+}
