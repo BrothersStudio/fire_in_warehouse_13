ec4881d9991814141f59c0a28b2e4202d967a491
whatPanache <pwcarney@bu.edu>
2017-01-06 22:41:39 -0500

    Some monster stuff

--- fiery_ghost/Assets/Scripts/PlayerController.cs
+++ fiery_ghost/Assets/Scripts/PlayerController.cs
@@ -27,6 +27,7 @@ public class PlayerController : MonoBehaviour {
 
 	void Update()
 	{
+		// Rotate player to face mouse pointer
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
 
@@ -49,20 +50,14 @@ public class PlayerController : MonoBehaviour {
 			playerSpeed = playerSpeed * fireSlowdownFactor;
 		}
 	}
-
-	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+		
 	void FixedUpdate()
 	{
-		//Store the current horizontal input in the float moveHorizontal.
 		float moveHorizontal = Input.GetAxis ("Horizontal");
-
-		//Store the current vertical input in the float moveVertical.
 		float moveVertical = Input.GetAxis ("Vertical");
 
-		//Use the two store floats to create a new Vector2 variable movement.
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 
-		//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
 		rb2d.AddForce (movement * playerSpeed);
 	}
 
