3ca53704a5220b91458459083bf678d046b441a1
whatPanache <pwcarney@bu.edu>
2017-01-07 13:36:01 -0500

    Monster spawner logic added

--- fiery_ghost/Assets/Scripts/MonsterSpawnerController.cs
+++ fiery_ghost/Assets/Scripts/MonsterSpawnerController.cs
@@ -6,21 +6,35 @@ public class MonsterSpawnerController : MonoBehaviour {
 
 	public GameObject player;
 	public GameObject monsterPrefab;
+	public float monsterCooldown;
 	public float xSpawn;
 	public float ySpawn;
 
-	void Start () {
-		
-		Vector3 spawnLocation = new Vector3 (
-			Random.Range (-xSpawn, xSpawn), 
-			Random.Range (-ySpawn, ySpawn), 
-			0.0f);
+	private float cooldownTime;
+	public bool monsterExists;
 
-		GameObject newMonster = Instantiate (monsterPrefab, spawnLocation, player.GetComponent<Transform>().rotation);
-		newMonster.GetComponent<MonsterController> ().player = player;
+	void Start () 
+	{
+		cooldownTime = Time.time + monsterCooldown;
 	}
 
-	void Update () {
-		
+	void Update () 
+	{
+		if (!monsterExists && Time.time >= cooldownTime) 
+		{
+			Debug.Log("Spawned in monster");
+
+			monsterExists = true;
+			cooldownTime = Time.time + monsterCooldown;
+
+			Vector3 spawnLocation = new Vector3 (
+				                       Random.Range (-xSpawn, xSpawn), 
+				                       Random.Range (-ySpawn, ySpawn), 
+				                       0.0f);
+
+			GameObject newMonster = Instantiate(monsterPrefab, spawnLocation, player.GetComponent<Transform> ().rotation, this.transform);
+			newMonster.GetComponent<MonsterController> ().player = player;
+		}
+
 	}
 }
