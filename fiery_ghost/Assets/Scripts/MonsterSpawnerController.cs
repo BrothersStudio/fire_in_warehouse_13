abf42cb5b56d6638bd6273156e2a8aa5196bb3a5
whatPanache <pwcarney@bu.edu>
2017-01-07 01:14:08 -0500

    Added monster spawner and refined monster prefab

---
+++ fiery_ghost/Assets/Scripts/MonsterSpawnerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawnerController : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject monsterPrefab;
+	public float xSpawn;
+	public float ySpawn;
+
+	void Start () {
+		
+		Vector3 spawnLocation = new Vector3 (
+			Random.Range (-xSpawn, xSpawn), 
+			Random.Range (-ySpawn, ySpawn), 
+			0.0f);
+
+		GameObject newMonster = Instantiate (monsterPrefab, spawnLocation, player.GetComponent<Transform>().rotation);
+		newMonster.GetComponent<MonsterController> ().player = player;
+	}
+
+	void Update () {
+		
+	}
+}
