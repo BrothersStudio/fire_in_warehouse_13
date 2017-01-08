35caf761ad147a1833bd3cb963284aca8dbf38cf
kevincarney <krcarney@vt.edu>
2017-01-08 18:07:17 -0500

    Music changes at 75% health

---
+++ fiery_ghost/Assets/Scripts/MusicController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioMixerSnapshot start;
+    public AudioMixerSnapshot stringsIn;
+
+
+
+    public Image hp_bars_0;
+    public float stringsAmount = 0.75f;
+    private float fadeInTime = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	void FixedUpdate ()
+    {
+        if (hp_bars_0.fillAmount < stringsAmount)
+        {
+            stringsIn.TransitionTo(fadeInTime);
+        } 		
+	}
+
+}
