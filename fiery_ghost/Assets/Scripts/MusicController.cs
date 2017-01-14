1514b7073e01866a4ab43b866b0fc5c48647f1bd
whatPanache <pwcarney@bu.edu>
2017-01-13 22:17:31 -0500

    Nice healthbars, mate

--- fiery_ghost/Assets/Scripts/MusicController.cs
+++ fiery_ghost/Assets/Scripts/MusicController.cs
@@ -11,9 +11,9 @@ public class MusicController : MonoBehaviour
     public AudioMixerSnapshot stringsIn;
     public AudioClip spookyNoise;
 
+    public Image player_hp;
+	public Image house_hp;
 
-
-    public Image hp_bars_0;
     private float stringsAmount = 0.75f;
     private float fadeInTime = 0.3f;
     private float spookyEffectAmount = 0.60f;
@@ -29,13 +29,13 @@ public class MusicController : MonoBehaviour
 	void FixedUpdate ()
     {
         //when HP hits designated amt, switch to new snapshot
-        if (hp_bars_0.fillAmount < stringsAmount)
+		if (player_hp.fillAmount < stringsAmount || house_hp.fillAmount < stringsAmount)
         {
             stringsIn.TransitionTo(fadeInTime);
         }
         //when HP reaches certain threshold, and the noise hasn't played before, play the spooky noise
         //and make sure it doesn't play again
-        if (hp_bars_0.fillAmount < spookyEffectAmount && !hasPlayed)
+		if ((player_hp.fillAmount < stringsAmount || house_hp.fillAmount < stringsAmount) && !hasPlayed)
         {
             effectsSource.clip = spookyNoise;
             effectsSource.Play();
